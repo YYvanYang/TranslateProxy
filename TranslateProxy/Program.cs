67cb23013b5e71e4f96885cfd04f0b0c7438b937
Yvan Yang <ybyang@best-inc.com>
2022-11-10 20:17:22 +0800

    add cors config

--- TranslateProxy/Program.cs
+++ TranslateProxy/Program.cs
@@ -1,7 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "_myAllowSpecificOrigins",
+        policy =>
+        {
+            policy
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
+
+app.UseCors("_myAllowSpecificOrigins");
 
 app.MapReverseProxy();
 
